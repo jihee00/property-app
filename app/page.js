f97b9a08097dc5ab6ab322572fcafcac222d73c2
jihee Kim <jkim541@myseneca.ca>
2024-06-05 05:18:39 -0400

    Create MongoDB

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 const HomePage = () => {
+  console.log(process.env.MONGODB_URI);
+
   return (
     <div>
       <h1 className="text-3xl">Welcome</h1>
