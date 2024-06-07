80a0a12df336849807493608d251ce229384e0a7
jihee Kim <jkim541@myseneca.ca>
2024-06-06 23:39:46 -0400

    Home Property Listings

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
+import HomeProperties from '@/components/HomeProperties';
 
 const HomePage = () => {
   console.log(process.env.MONGODB_URI);
@@ -9,6 +10,7 @@ const HomePage = () => {
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   )
 }
