ff75ab30c8b739220d90321f67e92674dde3ea82
jihee Kim <jkim541@myseneca.ca>
2024-06-07 23:02:07 -0400

    Database Connection

--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
+import connectDB from '@/config/database';
 
-const HomePage = () => {
-  console.log(process.env.MONGODB_URI);
+const HomePage = async () => {
+  await connectDB();
+  //console.log(process.env.MONGODB_URI);
 
   return (
     <>
